c3449ef974ea3d60108b6e16361c38e15153becd
Moonika1991 <monika9107@gmail.com>
2020-09-24 09:27:40 +0200

    index page created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request):
+    return HttpResponse("I'm a single post page.")
